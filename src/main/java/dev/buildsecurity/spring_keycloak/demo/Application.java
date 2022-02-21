72103c7b73441ce7c96306a8bcc34c5ff81da6af
Alex Mor <5476113+nashcontrol@users.noreply.github.com>
2022-02-21 23:44:34 +0200

    Initial commit

---
+++ src/main/java/dev/buildsecurity/spring_keycloak/demo/Application.java
@@ -0,0 +1,15 @@
+package dev.buildsecurity.spring_keycloak.demo;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(KeycloakSpringBootProperties.class)
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/test/java/dev/buildsecurity/spring_keycloak/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.buildsecurity.spring_keycloak.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
