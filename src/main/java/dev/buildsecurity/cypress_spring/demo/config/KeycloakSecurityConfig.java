8b510b27d4b35a8d581bfe813a22f1e908ff2e49
alex.mor <nashcontrol@users.noreply.github.com>
2022-02-21 23:48:03 +0200

    package name change

---
+++ src/main/java/dev/buildsecurity/cypress_spring/demo/config/KeycloakSecurityConfig.java
@@ -0,0 +1,49 @@
+package dev.buildsecurity.cypress_spring.demo.config;
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+    Logger logger = LoggerFactory.getLogger(KeycloakSecurityConfig.class);
+
+    @Autowired
+    public void configureGlobal(final AuthenticationManagerBuilder auth) {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        super.configure(http);
+
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/anonymous").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .anyRequest().denyAll().and()
+                .oauth2ResourceServer().jwt();
+    }
+
+}
---
+++ src/main/java/dev/buildsecurity/cypress_spring/demo/controller/UserController.java
@@ -0,0 +1,29 @@
+package dev.buildsecurity.cypress_spring.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/user")
+public class UserController {
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping(value = "/anonymous", method = RequestMethod.GET)
+    public ResponseEntity<String> getAnonymous() {
+        return ResponseEntity.ok("Hello Anonymous user");
+    }
+
+    @PreAuthorize("hasRole('Reader')")
+    @RequestMapping(value = "/me", method = RequestMethod.GET)
+    public ResponseEntity<String> getUser(Authentication authentication) {
+        return ResponseEntity.ok("Hello " + authentication.getName());
+    }
+
+}
